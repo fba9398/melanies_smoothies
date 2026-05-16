e568603ec7c76886c4f4280a3939472085a31557
fba9398 <35268953+fba9398@users.noreply.github.com>
2026-05-16 10:55:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 
 # Import python packages.
-from snowflake.snowpark.context import get_active_session
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -13,7 +12,8 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 # FIX: Select the column on the Snowpark dataframe BEFORE passing it to st.dataframe
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
